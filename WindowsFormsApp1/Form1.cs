b5272f70bc6344ed80d5bbd14d7c30909709c563
KBB <kadirberabilgin616161@gmail.com>
2025-08-25 15:48:15 +0300

    WindowsFormsApp1
    Bu örnekte Windows Formslara giriş yapıp, textbox, label ve button kullanımını öğreniyoruz. Örnekte kullanıcıdan textboxlar ile veri alıp (kişinin ismi, soyismi ve mesleğini) bunları labellar ile gösteriyoruz.

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/*
+ 
+    Ders 6 - Bu Uygulamanın Amacı : Kullanıcının ad, soyad ve meslek bilgisini textbox ile al. Sonra textboxtaki bilgileri labele aktar.
+ 
+*/
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        ///     SOL UST TARAF      /// 
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        ///     SOL UST TARAF      ///     
+
+        ///     SAG ALT TARAF      ///
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label4.Text = textBox1.Text;
+            label5.Text = textBox2.Text;
+            label6.Text = textBox3.Text;
+        }
+
+        ///     SAG ALT TARAF      ///
+
+    }
+}
